3e5cc5b5a86b6edaac5449e4edf048687750f39c
RajaOad <130591119+RajaOad@users.noreply.github.com>
2023-06-08 18:41:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-window.MY_API_KEY = 'pub_24014699004815b7b4e1318d93e8897183ede';
+window.API_KEY_1 = 'pub_24014699004815b7b4e1318d93e8897183ede';
+window.API_KEY_2 = 'pub_240193d6ef9e952e9688caeac87a1739b8e7a';
+
